e8c6b69ceca380d10d8f3c99e9f9dbe05c730836
Manav Misra <manavm@visionwebsoft.com>
2021-06-03 06:51:35 -0500

    Add history button with fetched weather on click

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
+import { fetchCurrentWeather, fetchForecast } from './api.js';
 
+const history = document.querySelector('#history');
+
+function renderHistoryButton(city) {
+  history.innerHTML += `<li class="list-group-item text-center"><button class="bg-secondary text-light">${city}</button></li>`;
+}
+
+document.querySelector('form').addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const city = event.target.elements[0].value;
+
+  renderHistoryButton(city);
+
+  const weather = await fetchCurrentWeather(city);
+});
+
+// Bubbling 🧋
+document.querySelector('#history').addEventListener('click', async event => {
+  const weather = await fetchCurrentWeather(event.target.innerText);
+});
