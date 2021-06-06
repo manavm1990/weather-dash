468453d0ce4494a504d81005c8f901ab44e9ffbd
Manav Misra <manavm@visionwebsoft.com>
2021-06-05 20:42:16 -0500

    Add fucking left margin to stupid h2

--- lib.js
+++ lib.js
@@ -104,6 +104,7 @@ export const renderForecast = forecast => {
   const forecastSection = document.querySelector('#forecast');
   const h2 = document.createElement('h2');
 
+  h2.classList.add('ms-2');
   h2.innerText = '5-Day Forecast';
   forecastSection.appendChild(h2);
 
