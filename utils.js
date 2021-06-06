91255ed9a929f62ecd3ea5c666e2891ff285d5d6
Manav Misra <manavm@visionwebsoft.com>
2021-06-06 06:36:35 -0500

    :fire: getDateWithAddlDays

--- utils.js
+++ utils.js
@@ -4,11 +4,3 @@ export function capitalizeEachWord(str) {
     .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(' ');
 }
-
-export function getDateWithAddlDays(addlDays = 0) {
-  const now = new Date(Date.now());
-
-  return `${now.getMonth() + 1}/${
-    now.getDate() + addlDays
-  }/${now.getFullYear()}`;
-}
